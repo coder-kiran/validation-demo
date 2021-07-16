edbb91f7573c8228ca2ae9f494bf1dd29932ecfb
Kiran K K <kirankalak4@gmail.com>
2021-07-16 18:56:21 +0530

    initial commit

---
+++ src/components/AddUser.js
@@ -0,0 +1,12 @@
+import react from "react";
+
+const AddUser = () => {
+  <form>
+    <label htmlFor="username">Username</label>
+    <input id="username" type="text" />
+    <label htmlFor="age">Age(years</label>
+    <input id="age" type="number" />
+  </form>;
+};
+
+export default AddUser;
